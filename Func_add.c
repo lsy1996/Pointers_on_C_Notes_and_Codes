85b91e69fea85e3ba70918292ccce32ed36306b7
lsy1996 <244055962@qq.com>
2022-09-17 22:52:23 +0800

    try to use extern

---
+++ Func_add.c
@@ -0,0 +1,6 @@
+extern int funcadd(int x, int y);
+
+int funcadd(int x, int y)
+{
+    return x+y;
+}
--- Hello World.c
+++ Hello World.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <windows.h>
+#include "Func_add.c"
 
 int main ()
 {
+   int z;
    printf("Hello, world!\n");
    printf("How do you do?\n");
    printf("I'm doing great!\n");
    printf("00000000000\n");
    printf("00000000000\n");
+
+   z = funcadd(3, 5);
+   printf("3 + 5 = %d\n", z);
+
    system("pause");
    return 0;
 }
